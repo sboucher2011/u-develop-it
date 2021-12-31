7ee20bc2770057d00c60947073826b2e40fdbda7
sboucher <sboucher2011@gmail.com>
2021-12-31 13:12:58 -0500

    added some options to server.js

--- server.js
+++ server.js
@@ -31,7 +31,39 @@ app.use((req, res) => {
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`);
 
+    /*
     db.query(`SELECT * FROM candidates`, (err, rows) => {
         console.log(rows);
-      });
+    });
+
+    // GET a single candidate
+    db.query(`SELECT * FROM candidates WHERE id = 1`, (err, row) => {
+        if (err) {
+        console.log(err);
+        }
+        console.log(row);
+    });
+
+    // Delete a candidate
+    db.query(`DELETE FROM candidates WHERE id = ?`, 20, (err, result) => {
+    if (err) {
+      console.log(err);
+    }
+    console.log(result);
+  });
+
+  // Create a candidate
+    const sql = `INSERT INTO candidates (id, first_name, last_name, industry_connected) 
+    VALUES (?,?,?,?)`;
+    const params = [1, 'Ronald', 'Firbank', 1];
+
+    db.query(sql, params, (err, result) => {
+    if (err) {
+    console.log(err);
+    }
+    console.log(result);
+    });
+
+*/
+
 });
